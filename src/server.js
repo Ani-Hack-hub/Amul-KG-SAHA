791f566d34d9065bf4a839765dcf29328996649d
Ani-Hack-hub <aniruddhanarasimman@gmail.com>
2024-02-26 17:40:52 +0545

    modified server.js

--- src/server.js
+++ src/server.js
@@ -1,22 +1,2 @@
-import { MongoClient } from "mongodb";
+import mongoose from "mongoose";
 
-const uri = "mongodb://localhost:27017/";
-const connection = new MongoClient(uri);
-
-async function database() {
-  try {
-    await connection.connect();
-    const db = connection.db("new_tab");
-    const coll = db.collection("new_tab");
-
-    // Using toArray() to await the result
-    const result = await coll.find().toArray();
-    console.log(result);
-  } catch (err) {
-    console.error(err);
-  } finally {
-    connection.close();
-  }
-}
-
-export default database;
