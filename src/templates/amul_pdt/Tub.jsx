032af01d185462c5120564dfa52926cfefa2190c
Ani-Hack-hub <aniruddhanarasimman@gmail.com>
2024-03-19 18:22:08 +0530

    modified tub

--- src/templates/amul_pdt/Tub.jsx
+++ src/templates/amul_pdt/Tub.jsx
@@ -1,81 +1,56 @@
-import "../../assets/static/tub.css"
+import "../../assets/static/tub.css";
 
-function Tub(){
-    return <section className="tub">
-    <ul>
-      <li>
-        <div
-          className="tub-item"
-          id="choco_chip"
-          ></div>
+function Tub() {
+  return (
+    <section className="tub">
+      <ul>
+        <li>
+          <div className="tub-item" id="choco_chip"></div>
 
-        <div className="cont" id="choco_chip_cont">
-          <span>AMUL CHOCH CHIP 1L</span><br />
-          1L <br />
-          a delicious harmony of real milk along with choco chips.
-        </div>
-      </li>
-      <li>
-        <div
-          className="tub-item"
-          id="choco_browine"
-          ></div>
-        <div className="cont" id="choco_browine_cont"><span>AMUL CHOCO BROWNIE 1L</span><br />
-          a delicious harmony of real milk along with Richness of Amul
-          Chocolate mixed with chocolate brownie.
-        </div>
-      </li>
-      <li>
-        <div
-          className="tub-item"
-          id="fnt"
-          ></div>
-        <div className="cont" id="fnt_cont"><span>AMUL FRUIT AND NUT 1L</span><br />
-          a delicious harmony of real milk along with choco chips, cashew nut
-          and tangy orange jelly and raisins.
-        </div>
-      </li>
-      <li>
-        <div
-          className="tub-item"
-          id="king_aphan"
-          ></div>
-        <div className="cont" id="king_aphan_cont"><span>AMUL KING ALPHANSO MANGO 1L</span><br />
-          a delicious harmony of real milk along with real Alphonso mango
-          pulp.
-        </div>
-      </li>
-      <li>
-        <div
-          className="tub-item"
-          id="moroccan_tub"
-          ></div>
-        <div className="moroccan_tub_cont" id="moroccan_tub_cont"><span>AMUL MOROCCAON DRY FRUIT 1L</span><br />
-          a delicious harmony of real milk along with almond and pistachio
-          nuts.
-        </div>
-      </li>
-      <li>
-        <div
-          className="tub-item"
-          id="rajbhoj_tub"
-          ></div>
-        <div className="cont" id="rajbhoj_tub_cont"><span>AMUL RAJBHOJ 1L</span><br />
-          a delicious harmony of real milk along with almond, cashew nuts and
-          immunity boosting honey and saffron.
-        </div>
-      </li>
-      <li>
-        <div
-          className="tub-item"
-          id="roasted_almon"
-          ></div>
-        <div className="cont" id="roasted_almon_cont"><span>AMUL ROASTED ALMOND 1L</span><br />
-          a delicious harmony of real milk along with nutty roasted almonds.
-        </div>
-      </li>
-    </ul>
-  </section>
+          <div className="tub-cont" id="choco_chip_cont">
+            <span>AMUL CHOCH CHIP 1L</span>
+            <br />
+          </div>
+        </li>
+        <li>
+          <div className="tub-item" id="choco_browine"></div>
+          <div className="tub-cont" id="choco_browine_cont">
+            <span>AMUL CHOCO BROWNIE 1L</span>
+          </div>
+        </li>
+        <li>
+          <div className="tub-item" id="fnt"></div>
+          <div className="tub-cont" id="fnt_cont">
+            <span>AMUL FRUIT AND NUT 1L</span>
+          </div>
+        </li>
+        <li>
+          <div className="tub-item" id="king_aphan"></div>
+          <div className="tub-cont" id="king_aphan_cont">
+            <span>AMUL KING ALPHANSO MANGO 1L</span>
+          </div>
+        </li>
+        <li>
+          <div className="tub-item" id="moroccan_tub"></div>
+          <div className="tub-cont" id="moroccan_tub_cont">
+            <span>AMUL MOROCCAON DRY FRUIT 1L</span>
+          </div>
+        </li>
+        <li>
+          <div className="tub-item" id="rajbhoj_tub"></div>
+          <div className="tub-cont" id="rajbhoj_tub_cont">
+            <span>AMUL RAJBHOJ 1L</span>
+          </div>
+        </li>
+        <li>
+          <div className="tub-item" id="roasted_almon"></div>
+          <div className="tub-cont" id="roasted_almon_cont">
+            <span>AMUL ROASTED ALMOND 1L</span>
+          </div>
+        </li>
+      </ul>
+    </section>
+  );
 }
 
-export default Tub
+export default Tub;
